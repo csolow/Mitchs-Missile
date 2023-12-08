8a811baf0640034f59fe67b3e392f425fa622598
Conrad Solow <conradssolow@gmail.com>
2023-12-08 15:12:59 +0100

    har lavede folders under prefabs og added en ikke færdig options page

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -15,5 +15,12 @@ func _on_training_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
+
+
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+
