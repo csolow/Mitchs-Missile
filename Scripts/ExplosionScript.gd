16f1446585ebe5e8d5d880e06790e9c1225c87cd
Valdeman <valdemar.ag.winther@gmail.com>
2023-12-28 14:24:51 +0100

    Firework Particles

--- Scripts/ExplosionScript.gd
+++ Scripts/ExplosionScript.gd
@@ -1,21 +1,27 @@
 extends Area2D
-@export var cooldown = 5.0
+@export var cooldown = 0.0
 var cooldown_counter = 0
+
+var explosion_complete = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if get_node("GPUParticles2D").lifetime < cooldown:
+		cooldown = get_node("GPUParticles2D").lifetime
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (cooldown_counter >= cooldown):
+		explosion_complete = true
+	if (cooldown_counter >= get_node("GPUParticles2D").lifetime):
 		queue_free()
 	cooldown_counter += delta
 	pass
 
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and not explosion_complete:
 		body.on_explosion_body_entered(self)
 	pass # Replace with function body.
 
---
+++ Scripts/FireworkParticles.gd
@@ -0,0 +1,14 @@
+extends GPUParticles2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	emitting = true;
+	one_shot = true;
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
