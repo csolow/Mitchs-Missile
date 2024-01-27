75a24cb41d6e52c5853e393b34bc85da29c922c9
Valdeman <valdemar.ag.winther@gmail.com>
2024-01-27 22:18:44 +0100

    fixed error message about menuhandler

--- Scripts/MenuScripts/MenuHandler.gd
+++ Scripts/MenuScripts/MenuHandler.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name MenuHandler
 
 var showing_pause_menu = false
-@onready var root = get_tree().get_node("Main")
+#@onready var root = get_tree().get_node("Main")
 
 
 # Called when the node enters the scene tree for the first time.
